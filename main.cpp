572c8f7a801dcf58862f8d6f8c3fe392e67adca4
Sara Rahmati <srahmati683@gmail.com>
2024-06-25 17:24:26 +0330

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <QApplication>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QTimer>
+#include <QDebug>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    QGraphicsView view;
+    QGraphicsScene scene;
+
+    view.setScene(&scene);
+    view.setFixedSize(800, 600);
+
+    // Add player
+    QGraphicsRectItem* player = new QGraphicsRectItem(0, 0, 32, 32);
+    player->setBrush(Qt::blue);
+    player->setPos(50, 50);
+    scene.addItem(player);
+
+    // Add platforms
+    std::vector<QGraphicsRectItem*> platforms = {
+        new QGraphicsRectItem(0, 0, 100, 20),
+        new QGraphicsRectItem(0, 0, 100, 20),
+        new QGraphicsRectItem(0, 0, 100, 20)
+    };
+    platforms[0]->setPos(0, 100);
+    platforms[1]->setPos(150, 150);
+    platforms[2]->setPos(300, 200);
+    for (auto& platform : platforms) {
+        platform->setBrush(Qt::gray);
+        scene.addItem(platform);
+    }
+
+    view.show();
+
+    qDebug() << "Application started with player and platforms";
+
+    QTimer timer;
+    QObject::connect(&timer, &QTimer::timeout, [&]() {
+        static int velocityY = 0;
+        static int posX = 50;
+        static int posY = 50;
+
+        // Gravity
+        velocityY += 1;
+        posY += velocityY;
+
+        // Movement
+        posX += 1;
+
+        // Update player position
+        player->setPos(posX, posY);
+
+        qDebug() << "Player position: (" << posX << "," << posY << ")";
+    });
+    timer.start(16); // 60 FPS
+
+    return a.exec();
+}
